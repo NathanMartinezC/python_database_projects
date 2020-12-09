403b1203ff8ba0bda61961e5bc6f1d74e82b0937
Nathán <n.mtz.cas@gmail.com>
2020-12-09 15:05:35 -0600

    initial prog_diary

---
+++ prog_diary/app.py
@@ -0,0 +1,39 @@
+from database import add_entry, get_entries
+
+
+menu = """Please select one of the following options:
+1) add new entry for today.
+2) View entries.
+3) Exit.
+
+Your selection: """
+
+welcome = "Welcome to the programming diary!"
+
+def prompt_new_entry():
+    entry_content = input("what have you learned today? ")
+    entry_date = input("Enter the date: ")
+    add_entry(entry_content, entry_date)
+
+def view_entries(entries):
+    for entry in entries:
+        print(f"{entry['date']}\n{entry['content']}\n\n")
+
+
+# entries = [
+#     {'content': 'Today I started learning programing', 'date': '01-01-2020'},
+#     {'content': 'I created my first SQLite database!', 'date': '02-01-2020'},
+#     {'content': 'I finished writing my programming diary application', 'date': '03-01-2020'},
+#     {'content': 'Today Im going to continue learning programming', 'date': '04-01-2020'},
+# ]
+
+while (user_input := input(menu)) != "3":
+    if user_input == "1":
+        prompt_new_entry()
+    elif user_input == "2":
+        view_entries(get_entries())
+    else:
+        print("invalid option, please try again!")
+
+
+
